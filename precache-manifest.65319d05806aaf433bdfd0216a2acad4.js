f8d3f9806c28f80d0af165da8ef22eb28fc11cad
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-12-09 11:45:46 +0100

    e45f6ee

---
+++ precache-manifest.65319d05806aaf433bdfd0216a2acad4.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7217a81432bcd14bc50f",
+    "url": "facial-landmark-annotation/css/app.bdb6ac97.css"
+  },
+  {
+    "revision": "4737a5d2c18b0a2be4b6",
+    "url": "facial-landmark-annotation/css/chunk-vendors.2aeb5162.css"
+  },
+  {
+    "revision": "038c5a2a0ffbdbb9a7961749c8e88e4c",
+    "url": "facial-landmark-annotation/index.html"
+  },
+  {
+    "revision": "7217a81432bcd14bc50f",
+    "url": "facial-landmark-annotation/js/app.499cc35f.js"
+  },
+  {
+    "revision": "4737a5d2c18b0a2be4b6",
+    "url": "facial-landmark-annotation/js/chunk-vendors.0a39e2b2.js"
+  },
+  {
+    "revision": "84592a59f55cdd91a5078528fdf571b2",
+    "url": "facial-landmark-annotation/manifest.json"
+  },
+  {
+    "revision": "33ec63fec9fc41801930d44f4f4ea8f0",
+    "url": "facial-landmark-annotation/models/face_expression_model-shard1"
+  },
+  {
+    "revision": "1eee5a2eea5ea5652904a2af88333dc1",
+    "url": "facial-landmark-annotation/models/face_expression_model-weights_manifest.json"
+  },
+  {
+    "revision": "124304f06e07fcf928290ff776e96141",
+    "url": "facial-landmark-annotation/models/face_landmark_68_model-shard1"
+  },
+  {
+    "revision": "1d4029763003335bc6921aadeb58706a",
+    "url": "facial-landmark-annotation/models/face_landmark_68_model-weights_manifest.json"
+  },
+  {
+    "revision": "47047fee26557b55d985952bdfc6cba1",
+    "url": "facial-landmark-annotation/models/face_landmark_68_tiny_model-shard1"
+  },
+  {
+    "revision": "29ea9c5c0e59a3069f8999b4ba1bd173",
+    "url": "facial-landmark-annotation/models/face_landmark_68_tiny_model-weights_manifest.json"
+  },
+  {
+    "revision": "4364fd63a666c7c885a77dec42ce3b42",
+    "url": "facial-landmark-annotation/models/face_recognition_model-shard1"
+  },
+  {
+    "revision": "f85f8daa5fc0beac4c3b9844cd435b7f",
+    "url": "facial-landmark-annotation/models/face_recognition_model-shard2"
+  },
+  {
+    "revision": "4e77deafcf99669abe6e21070d341451",
+    "url": "facial-landmark-annotation/models/face_recognition_model-weights_manifest.json"
+  },
+  {
+    "revision": "846e8dd4e6a7ed12a2c749324adf0e11",
+    "url": "facial-landmark-annotation/models/mtcnn_model-shard1"
+  },
+  {
+    "revision": "781bd744a5399d7cb0516d2a59a7c1c0",
+    "url": "facial-landmark-annotation/models/mtcnn_model-weights_manifest.json"
+  },
+  {
+    "revision": "37ef238973ea93daac91f1914478c40b",
+    "url": "facial-landmark-annotation/models/ssd_mobilenetv1_model-shard1"
+  },
+  {
+    "revision": "b6d5e81e2506145360be5c4278067080",
+    "url": "facial-landmark-annotation/models/ssd_mobilenetv1_model-shard2"
+  },
+  {
+    "revision": "cd2d65ec62107ba72b8b8d5047011647",
+    "url": "facial-landmark-annotation/models/ssd_mobilenetv1_model-weights_manifest.json"
+  },
+  {
+    "revision": "2e48b20953b0c59df47459d0319843a0",
+    "url": "facial-landmark-annotation/models/tiny_face_detector_model-shard1"
+  },
+  {
+    "revision": "5bab50532388f5da9b4cd85b15adc11c",
+    "url": "facial-landmark-annotation/models/tiny_face_detector_model-weights_manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "facial-landmark-annotation/robots.txt"
+  }
+]);
